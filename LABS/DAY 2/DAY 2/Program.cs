430c8192cc0acb2db85ee7f3e9d7a39202709fb2
Abhicodes55 <abhispeaks55@gmail.com>
2022-09-01 15:07:25 +0530

    adding conditional statement example

---
+++ LABS/DAY 2/DAY 2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace DAY_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter two numbers to check for the greater number: ");
+            int first, second;
+            first = Convert.ToInt32(Console.ReadLine());
+            second = Convert.ToInt32(Console.ReadLine());
+            if (first>second)
+            {
+                Console.WriteLine("{0} is greater than {1}",first,second);
+            }
+            else
+            {
+                Console.WriteLine("{0} is greater than {1}", second, first );
+            }
+        }
+    }
+}
