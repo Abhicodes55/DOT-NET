b2363f99e8fdb728a93094501907ddc2fe2a8593
Abhicodes55 <abhispeaks55@gmail.com>
2022-09-06 13:23:18 +0530

    Added Abstract class and Constructor/Destructor Program

---
+++ LABS/DAY 5/DAY 5/Abstract Class.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace DAY_5
+{ 
+
+
+
+
+    abstract class Animal
+    {  public string name;
+        public void Getname()
+        {
+            Console.WriteLine("Enter name of Dog:");
+           
+        }
+        public void Setname()
+        {
+            name = Console.ReadLine();
+        }
+        abstract public void Eat();
+    }
+    class Dog:Animal
+    {  
+        override public void Eat()
+        {
+            Console.WriteLine( name + " is Eating !");
+        }
+        static void Main3(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.Getname();
+            d1.Setname();
+            d1.Eat();
+        }
+    }
+}
---
+++ LABS/DAY 5/DAY 5/Constructors and Destructors.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DAY_5
+{
+    public class ConstructorsAndDestructors
+    {
+        public static void Main()
+        {
+            int total = 3;
+            Person[] persons = new Person[total];
+            Console.WriteLine("Enter three Names : ");
+            for (int i = 0; i < total; i++)
+            {
+                persons[i] = new Person(Console.ReadLine());
+            }
+
+            for (int i = 0; i < total; i++)
+            {
+                Console.WriteLine(persons[i].ToString());
+            }
+        }
+
+        public class Person
+        {
+            public string Name { get; set; }
+
+            public Person(string name)
+            {
+                Name = name;
+            }
+
+            public override string ToString()
+            {
+                return "\nHello! My name is " + Name;
+            }
+
+            ~Person()
+            {
+                Name = string.Empty;
+            }
+        }
+    }
+}
