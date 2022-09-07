eea43a71194275dc4583c30eb949fd3e369a21b7
Abhicodes55 <abhispeaks55@gmail.com>
2022-09-07 09:38:38 +0530

    Added mini projects

---
+++ LABS/Mini Project (Day 5)/Mini Project (Day 5)/Photobook Project.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mini_Project__Day_5_
+{
+    class Photobook 
+    {
+        protected int pages;
+        public void Getpages(int pages)
+        {
+            
+            Console.WriteLine("There are "+pages + " Number of pages in Album");  
+        }
+       public Photobook (int pages = 16)
+        {
+            Console.WriteLine("There are "+pages + " Number of pages in Album");
+        }
+      
+    }
+    class Bigphotobook
+    {
+        public Bigphotobook(int pages = 64)
+        {
+            Console.WriteLine("There are "+ pages + " Number of pages in Album");
+        }
+    }
+    class M
+    {
+        public static void Main()
+        {
+            Photobook P = new Photobook();
+            Console.WriteLine("Enter the number of pages in Album: ");
+            P.Getpages(Convert.ToInt32(Console.ReadLine()));
+            Bigphotobook B = new Bigphotobook();
+        }
+    }
+}
---
+++ LABS/Mini Project (Day 5)/Mini Project (Day 5)/miniproject.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+namespace Mini_Project__Day_5_
+{   
+    class Person
+    {
+        public int age;
+        public void Greet()
+        {
+            Console.WriteLine("hello ! ");
+        }
+        public void SetAge(int Age)
+        {
+            age = Age;
+
+        }
+
+    }
+    class Student : Person
+    {
+      public void Study()
+      {
+            Console.WriteLine("i am studying ");
+      }
+        public void Showage()
+        {
+            Console.WriteLine("My age is "+age+" years");
+        }
+    }
+
+
+    class Teacher : Person
+    {
+        public void Explain()
+        {
+            Console.WriteLine("i am Explaining ");
+        }
+
+    }
+    class Miniproject
+    {
+        static void Main()  
+        {
+            Person P = new Person();
+            P.Greet();
+            Console.WriteLine();
+            Student S = new Student();
+            S.SetAge(18);
+            S.Greet();
+            S.Showage();
+            S.Study();
+            Console.WriteLine();
+            Teacher T = new Teacher();
+            T.SetAge(45);
+            T.Greet();
+            T.Explain();
+
+        }
+    }
+}
